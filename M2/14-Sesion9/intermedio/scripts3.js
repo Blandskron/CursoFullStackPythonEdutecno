7dd3781a474fcd06959bf01508af205a3ec76e84
blandskron <bastianlandskronfreelancer@gmail.com>
2024-08-19 20:23:01 -0400

    update

--- M2/14-Sesion9/intermedio/scripts3.js
+++ M2/14-Sesion9/intermedio/scripts3.js
@@ -2,3 +2,7 @@
 var nuevoParrafo = document.createElement("p");
 nuevoParrafo.innerText = "Este es un nuevo párrafo.";
 document.getElementById("miDiv").appendChild(nuevoParrafo);
+
+var titulo = document.createElement("h2");
+titulo.innerText = "Este es un título.";
+document.getElementById("miTitulo").appendChild(titulo);
