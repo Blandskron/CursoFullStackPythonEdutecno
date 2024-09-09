d0bae340a3ed352da50077276c82d75cb94d45c0
blandskron <bastianlandskronfreelancer@gmail.com>
2024-09-09 00:03:20 -0300

    update

--- M3/15-Sesion8/3_ListasTuplasSetsDiccionarios.py
+++ M3/15-Sesion8/3_ListasTuplasSetsDiccionarios.py
@@ -1,15 +1,19 @@
 # Crear una lista
 mi_lista = [1, 2, 3, 4, 5]
 print("Lista:", mi_lista)
+mi_lista = [10, 20, 30]
 
 # Crear una tupla
 mi_tupla = (1, 2, 3, 4, 5)
 print("Tupla:", mi_tupla)
+mi_tupla = (10, 20, 30)
 
 # Crear un set
 mi_set = {1, 2, 3, 4, 5}
 print("Set:", mi_set)
+mi_set = {10, 20, 30}
 
 # Crear un diccionario
 mi_diccionario = {"nombre": "Juan", "edad": 25}
 print("Diccionario:", mi_diccionario)
+mi_diccionario = {"clave1": 10, "clave2": 20}
