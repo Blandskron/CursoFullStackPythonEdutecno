48bbaa24eaddb213d3587669f770498a31c04693
blandskron <bastianlandskronfreelancer@gmail.com>
2024-09-09 20:09:47 -0300

    update

---
+++ M3/10-Sesion5/Desafio1/funciones/flujo.py
@@ -0,0 +1,21 @@
+from funciones.importaciones import *
+
+
+def main():
+    # Operadores
+    operadores_logicos(True, False)
+    operadores_comparacion(10, 20)
+    comparaciones_entre_tipos(10, 10.5)
+    
+    # Conversiones implícitas
+    conversiones_implicitas()
+    
+    # Manipular variables y atributos
+    manipular_objetos()
+    
+    # Control de flujo
+    ejemplo_if_else(-5)
+    ejemplo_for_else([1, 2, 3])
+    ejemplo_while(3)
+    ejemplo_break_continue()
+    
