657e1c1c1465650d6300a138c4549b8d5815a1b1
blandskron <bastianlandskronfreelancer@gmail.com>
2024-09-12 20:50:51 -0300

    updtae

--- M3/13-Sesion7/6_WhileElse.py
+++ M3/13-Sesion7/6_WhileElse.py
@@ -4,3 +4,12 @@ while x < 5:
     x += 1
 else:
     print("El ciclo ha terminado")
+
+
+x = int(input("ingresa un numero: "))
+u = int(input("ingresa un numero: "))
+while x < u:
+    print(f"Valor de x: {x}")
+    x += 1
+else:
+    print("El ciclo ha terminado")
