105c086a46b932c38ed36bf6092d37295d5f8287
blandskron <bastianlandskronfreelancer@gmail.com>
2024-09-12 20:48:21 -0300

    updtae

--- M3/13-Sesion7/1_Sandwiches.py
+++ M3/13-Sesion7/1_Sandwiches.py
@@ -1,6 +1,18 @@
+# Harcodeada
 # Número de sándwiches necesarios
 cantidad_sandwiches = 500
 
 # Hacer sándwiches utilizando un ciclo
 for i in range(cantidad_sandwiches):
     print(f"Sándwich {i + 1}: Agregar pan, jamón, queso y cerrar.")
+
+# Codigo Interactivo
+# Número de sándwiches necesarios
+cantidad_sandwiches = int(input("Ingrese la cantidad de sándwiches: "))
+
+# Hacer sándwiches utilizando un ciclo
+for i in range(cantidad_sandwiches):
+    print("Extra = palta, carne, queso extra y tomate.")
+    ingrediente_extra = input(f"Ingrese el ingrediente extra para el sándwich {i + 1}: ")
+    print(f"Sándwich {i + 1}: Agregar pan, jamón, queso, Ingrediente extra {ingrediente_extra}")
+    
