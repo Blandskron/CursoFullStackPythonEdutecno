fbf9394f07288f8174b8d210d54f7f25be11c1fb
blandskron <bastianlandskronfreelancer@gmail.com>
2024-10-11 20:44:04 -0300

    update

--- M4/8-Sesion5/10_Args.py
+++ M4/8-Sesion5/10_Args.py
@@ -2,5 +2,5 @@ def sumar(*args):
     resultado = sum(args)
     print(f"Resultado: {resultado}")
 
-sumar(1, 2, 3)   # Resultado: 6
-sumar(10, 20)    # Resultado: 30
+sumar(1, 2, 3, 4)
+sumar(10, 20, 30, 40, 50, 20, 80, 90, 100)
--- M4/8-Sesion5/11_KwargsBasico.py
+++ M4/8-Sesion5/11_KwargsBasico.py
@@ -2,4 +2,16 @@ def mostrar_informacion(**kwargs):
     for clave, valor in kwargs.items():
         print(f"{clave}: {valor}")
 
-mostrar_informacion(nombre="Juan", edad=25, ciudad="Madrid")
+def mostrar_keys(**kwargs):
+    for clave in kwargs.keys():
+        print(f"{clave}")
+
+def mostrar_values(**kwargs):
+    for valor in kwargs.values():
+        print(f"{valor}")
+
+mostrar_informacion(nombre="Juan", segundo_nombre="Andres", apellido="Perez", segundo_apellido="Perez", edad=25, ciudad="Madrid")
+
+mostrar_keys(nombre="Juan", segundo_nombre="Andres", apellido="Perez", segundo_apellido="Perez", edad=25, ciudad="Madrid")
+
+mostrar_values(nombre="Juan", segundo_nombre="Andres", apellido="Perez", segundo_apellido="Perez", edad=25, ciudad="Madrid")
