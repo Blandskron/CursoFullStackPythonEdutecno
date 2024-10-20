1b93a4f2169cc736a32bb02a84fa62f28125f996
blandskron <bastianlandskronfreelancer@gmail.com>
2024-10-20 14:22:02 -0300

    update

--- M5/1-Sesion1/01_Definicion.sql
+++ M5/1-Sesion1/01_Definicion.sql
@@ -1,11 +1,11 @@
 -- Ejemplo 1: Creamos una base de datos para gestionar una librería.
 CREATE DATABASE LibreriaDB;
 
--- Usamos la base de datos creada
+-- Usamos la base de datos creada -- No Existe en PostgreSQL solo MYSQL
 USE LibreriaDB;
 
 -- Ejemplo 2: Creamos una base de datos para un sistema escolar.
 CREATE DATABASE EscuelaDB;
 
--- Usamos la base de datos creada
+-- Usamos la base de datos creada --- No Existe en PostgreSQL
 USE EscuelaDB;
