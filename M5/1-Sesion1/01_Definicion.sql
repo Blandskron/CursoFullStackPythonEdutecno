40b2dbb66881d60c94e8167f4005bc8bc37c3830
blandskron <bastianlandskronfreelancer@gmail.com>
2024-10-22 21:49:57 -0300

    update

--- M5/1-Sesion1/01_Definicion.sql
+++ M5/1-Sesion1/01_Definicion.sql
@@ -3,6 +3,8 @@ CREATE DATABASE LibreriaDB;
 
 -- Usamos la base de datos creada -- No Existe en PostgreSQL solo MYSQL
 USE LibreriaDB;
+-- Postgres
+ \c libreriadb
 
 -- Ejemplo 2: Creamos una base de datos para un sistema escolar.
 CREATE DATABASE EscuelaDB;
